ff845b14ddf1a732632544158702a83ac1a7107c
peppercopia <peppercopia@gmail.com>
2017-09-24 12:48:05 -0400

    Implemented the publishing of following events: - server start - server stop - player join - player quit
    Implemented the config file.

--- minecraft-sentry/src/ca/vigilem/minecraft/MinecraftSentry/SentryEventListener.java
+++ minecraft-sentry/src/ca/vigilem/minecraft/MinecraftSentry/SentryEventListener.java
@@ -1,15 +1,42 @@
 package ca.vigilem.minecraft.MinecraftSentry;
 
-import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class SentryEventListener implements Listener 
 {
-    @EventHandler
+	//Local objects to get the plugin configuration file
+	private JavaPlugin plugin;
+	private FileConfiguration config;
+	MessageBroker pubevent = new MessageBroker();
+	
+	@EventHandler
     public void onPlayerJoin(PlayerJoinEvent event)
     {
-    	Bukkit.broadcastMessage("This is where I am going to try and publish an event to MQTT.");
-    }
+	    plugin = Main.getInstance();
+	    config = plugin.getConfig();
+    	String PlayerName = event.getPlayer().getName();
+    	
+    	//Create the MQTT Event Payload and send it.
+    	String EventMessage = PlayerName + " has joined the game on server " + config.getString("mqtt-serverid") + "." ;
+    	pubevent.publishEvent(EventMessage);
+   	
+      }
+	
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event)
+	{
+	    plugin = Main.getInstance();
+	    config = plugin.getConfig();
+    	String PlayerName = event.getPlayer().getName();
+    	
+    	//Create the MQTT Event Payload and send it.
+    	String EventMessage = PlayerName + " has logged off server " + config.getString("mqtt-serverid") + "." ;
+    	pubevent.publishEvent(EventMessage);
+
+	}
 }
